510a1d0120a1bb663fc3ee7ed565f22e138d6e88
Nick B <nick.i.bonilla1@gmail.com>
2022-07-30 17:27:57 +0000

    add: income slider

--- visa-calculator/src/app/app.component.ts
+++ visa-calculator/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MatSliderChange } from '@angular/material/slider';
 
 @Component({
   selector: 'app-root',
@@ -19,7 +20,7 @@ export class AppComponent {
   pointDictionary = new Map<string, number>([['age' , 0], ['education' , 0], ['income' , 0], ['koreanLanguageAbility' , 0]])
   educationPoints = new Map<string, number>([['Doctorate Of Arts', 20], ['Doctorate of Science or Engineering', 25], ['Masters of Science or Engineering', 20], ['Masters of Arts', 17], ['Bachelor of Science or Engineering', 17], ['Bachelor of Arts', 15], ['Associate of Science or Engineering', 15], ['Associate of Arts', 10]])
   koreanLanguagePoints = new Map<string, number>([['Over Level 5', 20], ['Level 4', 15], ['Level 3', 10], ['Level 2', 5], ['Level 1', 3]])
-
+  incomePoints = new Map<number, number>([[0, 0], [23, 10], [30,30], [40, 40], [50, 45], [60, 50], [70, 53], [80, 56], [90, 58], [100, 60]])
   setPoints(categoryName: string, points: number){
     this.pointDictionary.set(categoryName, points);
   }
@@ -40,5 +41,26 @@ export class AppComponent {
   }
   getKoreanLevels() {
     return Array.from(this.koreanLanguagePoints.keys());
+  }
+  formatLabel(value: number) {   
+    return value + "M";
+  }
+  incomeMillions = 0;
+  setIncomeLevel(incomeMillions: number){
+     this.pointDictionary.set('income', this.incomePoints.get(incomeMillions)!);
+  }
+  incomeMillions3 = 0;
+  onInputChange($event: MatSliderChange){
+    if($event.value == null) return;
+    if($event.value < 23) this.setIncomeLevel(0);
+    if($event.value >= 23 && $event.value < 30) this.setIncomeLevel(23);
+    if($event.value >= 30 && $event.value < 40) this.setIncomeLevel(30);
+    if($event.value >= 40 && $event.value < 50) this.setIncomeLevel(40);
+    if($event.value >= 50 && $event.value < 60) this.setIncomeLevel(50);
+    if($event.value >= 60 && $event.value < 70) this.setIncomeLevel(60);
+    if($event.value >= 70 && $event.value < 80) this.setIncomeLevel(70);
+    if($event.value >= 80 && $event.value < 90) this.setIncomeLevel(80);
+    if($event.value >= 90 && $event.value < 100) this.setIncomeLevel(90);
+    if($event.value >= 100) this.setIncomeLevel(100);
   }
 }
