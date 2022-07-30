55aff961bf02c3247a910ee5d04779328fd010e1
Nick B <nick.i.bonilla1@gmail.com>
2022-07-30 06:54:50 +0000

    add: toggle buttons

--- visa-calculator/src/app/app.component.ts
+++ visa-calculator/src/app/app.component.ts
@@ -7,4 +7,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'visa-calculator';
+  x = 0;
+  incrementX() {
+      this.x = this.x + 1;
+  }
+  getX() {
+    return this.x;
+  }
+  //new Map<type, type>([[keyN, valueN]])
+  currentAgePoints = 0;
+  pointDictionary = new Map<string, number>([['age' , 0], ['education' , 0], ['income' , 0], ['koreanLanguageAbility' , 0]])
+  setPoints(categoryName: string, points: number){
+    this.pointDictionary.set(categoryName, points);
+  }
+  
+  getPoints() {
+    return Array.from(this.pointDictionary.values()).reduce((prev, curr) => curr + prev);
+  }
+
 }
--- visa-calculator/src/app/app.module.ts
+++ visa-calculator/src/app/app.module.ts
@@ -3,6 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
 
 @NgModule({
   declarations: [
@@ -10,7 +14,11 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatSliderModule,
+    MatButtonModule,
+    MatDividerModule,
+    MatButtonToggleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
